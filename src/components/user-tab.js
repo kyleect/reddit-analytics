e76f7b8e6ebaabdcc09f4944b86ec9ae20342921
Kylee Tilley <kyleetilley@gmail.com>
2017-12-09 23:09:17 -0600

    Adding UserProfile

--- src/components/user-tab.js
+++ src/components/user-tab.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { SearchTab } from "./utils/search-tab";
+import UserProfile from "./utils/user-profile";
+import { Segment } from "semantic-ui-react";
+import { SearchResults } from "./utils/search-results";
+import SearchForm from "./utils/search-form";
 
 export class UserTab extends SearchTab {
   constructor(props) {
@@ -13,5 +17,27 @@ export class UserTab extends SearchTab {
     } else {
       return 1;
     }
+  }
+
+  render() {
+    return (
+      <div>
+        <Segment>
+          <SearchForm label={this.props.label} onSubmit={this.onSubmitSearch} />
+        </Segment>
+
+        {this.state.results.length > 0 && (
+          <div>
+            <Segment>
+              <UserProfile userId={this.state.query} />
+            </Segment>
+
+            <Segment>
+              <SearchResults results={this.sortedResults} />
+            </Segment>
+          </div>
+        )}
+      </div>
+    );
   }
 }
---
+++ src/components/utils/user-profile.js
@@ -0,0 +1,79 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Segment, Statistic, Item } from "semantic-ui-react";
+
+async function fetchUser(user) {
+  if (user.length === 0) {
+    return;
+  }
+
+  const encodedUser = encodeURIComponent(`${user}`);
+
+  const requestUrl = `https://www.reddit.com/user/${encodedUser}/about.json`;
+
+  const response = await fetch(requestUrl, {
+    method: "GET",
+    headers: new Headers(),
+    mode: "cors",
+    cache: "default"
+  });
+
+  const json = await response.json();
+  const results = json.data;
+
+  return results;
+}
+
+class UserProfile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: undefined
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const user = await fetchUser(this.props.userId);
+      this.setState({ user });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.user ? (
+          <div>
+            <Item>
+              <Item.Image size="tiny" src={this.state.user.icon_img} />
+
+              <Item.Content>
+                <Item.Header>{this.state.user.name}</Item.Header>
+              </Item.Content>
+            </Item>
+
+            <Statistic>
+              <Statistic.Value>{this.state.user.comment_karma}</Statistic.Value>
+              <Statistic.Label>Comment Karma</Statistic.Label>
+            </Statistic>
+
+            <Statistic>
+              <Statistic.Value>{this.state.user.link_karma}</Statistic.Value>
+              <Statistic.Label>Link Karma</Statistic.Label>
+            </Statistic>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    );
+  }
+}
+
+UserProfile.propTypes = {
+  userId: PropTypes.string.isRequired
+};
+
+export default UserProfile;
